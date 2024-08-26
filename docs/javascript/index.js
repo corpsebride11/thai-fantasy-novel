f7e83f04ab76e8c280a3b74b16b8cf0d3e8c0f7f
corpsebride11 <cello9396@gmail.com>
2024-08-26 13:50:26 +0700

    create js file

---
+++ docs/javascript/index.js
@@ -0,0 +1,13 @@
+fetch('/resources/data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.employees);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
